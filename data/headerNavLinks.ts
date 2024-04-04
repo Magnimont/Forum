ffdc835cfba6a5c3084c92c32c22b7d3169b063b
Vikas Nath Jha <jhaakash177@gmail.com>
2024-04-05 04:11:20 +0530

    Initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Forum' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
